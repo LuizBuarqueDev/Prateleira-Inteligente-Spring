c43d2035aae3c9a32b98b0806b7a108322fb6604
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2024-12-23 01:05:27 -0300

    Adiçao do projeto spring

---
+++ Spring/src/main/java/com/prateleira_inteligente/prateleira_inteligente/PrateleiraInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.prateleira_inteligente.prateleira_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrateleiraInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrateleiraInteligenteApplication.class, args);
+	}
+
+}
---
+++ Spring/src/test/java/com/prateleira_inteligente/prateleira_inteligente/PrateleiraInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prateleira_inteligente.prateleira_inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrateleiraInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
