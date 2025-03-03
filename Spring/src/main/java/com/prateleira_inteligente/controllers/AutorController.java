f5490d9ee877b030e6c290dad2dcb513342e3b8d
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-03-03 14:07:59 -0300

    Pequenos ajustes

--- Spring/src/main/java/com/prateleira_inteligente/controllers/AutorController.java
+++ Spring/src/main/java/com/prateleira_inteligente/controllers/AutorController.java
@@ -12,19 +12,16 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/autor")
+@RequestMapping("api/autores")
 public class AutorController {
 
     private final AutorService autorService;
 
-    // Buscar todos os autores
-    @GetMapping
-    public ResponseEntity<List<AutorDTO>> findAll() {
-        List<AutorDTO> autorDTOList = autorService.findAll()
-                .stream()
-                .map(AutorDTO::toDTO)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(autorDTOList);
+    // Criar um novo autor
+    @PostMapping("/create")
+    public ResponseEntity<AutorDTO> create(@RequestBody AutorDTO autorDTO) {
+        Autor novoAutor = autorService.save(autorDTO.toEntity());
+        return ResponseEntity.ok(AutorDTO.toDTO(novoAutor));
     }
 
     // Buscar autor por ID
@@ -34,24 +31,28 @@ public class AutorController {
         return ResponseEntity.ok(AutorDTO.toDTO(autor));
     }
 
-    // Criar um novo autor
-    @PostMapping
-    public ResponseEntity<AutorDTO> create(@RequestBody AutorDTO autorDTO) {
-        Autor novoAutor = autorService.save(autorDTO.toEntity());
-        return ResponseEntity.ok(AutorDTO.toDTO(novoAutor));
-    }
 
     // Atualizar um autor existente
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<AutorDTO> update(@PathVariable Long id, @RequestBody AutorDTO autorDTO) {
         Autor autorAtualizado = autorService.update(id, autorDTO.toEntity());
         return ResponseEntity.ok(AutorDTO.toDTO(autorAtualizado));
     }
 
     // Excluir um autor por ID
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         autorService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // Buscar todos os autores
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> findAll() {
+        List<AutorDTO> autorDTOList = autorService.findAll()
+                .stream()
+                .map(AutorDTO::toDTO)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(autorDTOList);
     }
 }
