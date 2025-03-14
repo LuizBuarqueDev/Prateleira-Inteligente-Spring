7500d63ec40268d1abae0946331c74624f2c1216
Luiz_Buarque_Dev <luizgabrielbuarque@gmail.com>
2025-03-14 19:39:41 -0300

    Pequena correção

--- Spring/src/main/java/com/prateleira_inteligente/controllers/ComentarioController.java
+++ Spring/src/main/java/com/prateleira_inteligente/controllers/ComentarioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/Comentario")
+@RequestMapping("/api/comentario")
 public class ComentarioController extends AbsGenericController<Comentario, ComentarioDTO> {
 
     public ComentarioController(IService<Comentario> service, IMapper<Comentario, ComentarioDTO> mapper) {
